784d7aad4ecf8a7171bf8d5658843d7c3bc59356
Spencer Lindemuth <lindemut@gmail.com>
2019-04-03 17:56:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -71,7 +71,7 @@ def merge_data(keys, data)
   merged_array = []
   keys.each_with_index do |name, i|
     merged_array[i] = {first_name: name[:first_name],
-                      data[0][name[:first_name]]
+                      data[0][name[:first_name]],
                       motto: name[:motto]
                       }
 
