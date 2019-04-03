0fecaddec1c7688aaa3e9d404a779d6769cbe188
Spencer Lindemuth <lindemut@gmail.com>
2019-04-03 16:59:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ end
 def remove_non_strings(array)
   array.each_with_index do |element, i|
     binding.pry
-    if element.class != "String"
+    if element.class != String
       array.delete_at(i)
     end
   end
