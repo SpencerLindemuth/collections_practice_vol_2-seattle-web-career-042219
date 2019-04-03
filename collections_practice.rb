06b14c4f3883d313574a9bc171446460dbccb248
Spencer Lindemuth <lindemut@gmail.com>
2019-04-03 16:29:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,9 @@
 # your code goes here
 def begins_with_r(array)
   array.each do |i|
-    if i[0] == 'r' || 'R'
-      return true
+    if i[0] != 'r' || 'R'
+      return false
     end
   end
-  return false
+  return true
 end
