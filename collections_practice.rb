104b64f244b954383aae4de9bf421a3d546dff07
Spencer Lindemuth <lindemut@gmail.com>
2019-04-03 17:34:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,6 +52,7 @@ def count_elements(array)
   array.each do |element|
     if count_array.length > 0
       count_array.each_with_index do |x, i|
+        binding.pry
         if x[:name] == element[:name]
           num = count_array[i][:count]
           num += 1
